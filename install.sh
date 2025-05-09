79e46f175b67becaafc4509e7c48becf25fc9310
Anant <innovator.anant@gmail.com>
2025-05-09 21:21:31 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Define installation directory
-INSTALL_DIR="/opt/nitent-node-vps-optimizer"
+INSTALL_DIR="/opt/unique-node-vps-optimizer"
 
 # Cool Loading Animation
 function loading_animation() {
